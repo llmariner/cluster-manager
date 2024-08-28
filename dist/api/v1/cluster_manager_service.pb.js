96f2ffdc17546acf7addfd3176c8c3a6a5e63c57
Guangrui Fu <guangrui@cloudnatix.com>
2024-08-28 21:11:33 +0000

    generate ts files

---
+++ dist/api/v1/cluster_manager_service.pb.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+import * as fm from "../../fetch.pb";
+export class ClustersService {
+    static CreateCluster(req, initReq) {
+        return fm.fetchReq(`/v1/clusters`, Object.assign(Object.assign({}, initReq), { method: "POST", body: JSON.stringify(req) }));
+    }
+    static ListClusters(req, initReq) {
+        return fm.fetchReq(`/v1/clusters?${fm.renderURLSearchParams(req, [])}`, Object.assign(Object.assign({}, initReq), { method: "GET" }));
+    }
+    static GetCluster(req, initReq) {
+        return fm.fetchReq(`/v1/clusters/${req["id"]}?${fm.renderURLSearchParams(req, ["id"])}`, Object.assign(Object.assign({}, initReq), { method: "GET" }));
+    }
+    static DeleteCluster(req, initReq) {
+        return fm.fetchReq(`/v1/clusters/${req["id"]}`, Object.assign(Object.assign({}, initReq), { method: "DELETE" }));
+    }
+}
+export class ClustersInternalService {
+    static ListInternalClusters(req, initReq) {
+        return fm.fetchReq(`/llmoperator.clusters.server.v1.ClustersInternalService/ListInternalClusters`, Object.assign(Object.assign({}, initReq), { method: "POST", body: JSON.stringify(req) }));
+    }
+}
+export class ClustersWorkerService {
+    static GetSelfCluster(req, initReq) {
+        return fm.fetchReq(`/llmoperator.clusters.server.v1.ClustersWorkerService/GetSelfCluster`, Object.assign(Object.assign({}, initReq), { method: "POST", body: JSON.stringify(req) }));
+    }
+}
---
+++ ts/api/v1/cluster_manager_service.pb.ts
@@ -0,0 +1,79 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+
+import * as fm from "../../fetch.pb"
+export type Cluster = {
+  id?: string
+  name?: string
+  registrationKey?: string
+  object?: string
+}
+
+export type CreateClusterRequest = {
+  name?: string
+}
+
+export type ListClustersRequest = {
+}
+
+export type ListClustersResponse = {
+  object?: string
+  data?: Cluster[]
+}
+
+export type GetClusterRequest = {
+  id?: string
+}
+
+export type DeleteClusterRequest = {
+  id?: string
+}
+
+export type DeleteClusterResponse = {
+  id?: string
+  object?: string
+  deleted?: boolean
+}
+
+export type InternalCluster = {
+  cluster?: Cluster
+  tenantId?: string
+}
+
+export type ListInternalClustersRequest = {
+}
+
+export type ListInternalClustersResponse = {
+  clusters?: InternalCluster[]
+}
+
+export type GetSelfClusterRequest = {
+}
+
+export class ClustersService {
+  static CreateCluster(req: CreateClusterRequest, initReq?: fm.InitReq): Promise<Cluster> {
+    return fm.fetchReq<CreateClusterRequest, Cluster>(`/v1/clusters`, {...initReq, method: "POST", body: JSON.stringify(req)})
+  }
+  static ListClusters(req: ListClustersRequest, initReq?: fm.InitReq): Promise<ListClustersResponse> {
+    return fm.fetchReq<ListClustersRequest, ListClustersResponse>(`/v1/clusters?${fm.renderURLSearchParams(req, [])}`, {...initReq, method: "GET"})
+  }
+  static GetCluster(req: GetClusterRequest, initReq?: fm.InitReq): Promise<Cluster> {
+    return fm.fetchReq<GetClusterRequest, Cluster>(`/v1/clusters/${req["id"]}?${fm.renderURLSearchParams(req, ["id"])}`, {...initReq, method: "GET"})
+  }
+  static DeleteCluster(req: DeleteClusterRequest, initReq?: fm.InitReq): Promise<DeleteClusterResponse> {
+    return fm.fetchReq<DeleteClusterRequest, DeleteClusterResponse>(`/v1/clusters/${req["id"]}`, {...initReq, method: "DELETE"})
+  }
+}
+export class ClustersInternalService {
+  static ListInternalClusters(req: ListInternalClustersRequest, initReq?: fm.InitReq): Promise<ListInternalClustersResponse> {
+    return fm.fetchReq<ListInternalClustersRequest, ListInternalClustersResponse>(`/llmoperator.clusters.server.v1.ClustersInternalService/ListInternalClusters`, {...initReq, method: "POST", body: JSON.stringify(req)})
+  }
+}
+export class ClustersWorkerService {
+  static GetSelfCluster(req: GetSelfClusterRequest, initReq?: fm.InitReq): Promise<Cluster> {
+    return fm.fetchReq<GetSelfClusterRequest, Cluster>(`/llmoperator.clusters.server.v1.ClustersWorkerService/GetSelfCluster`, {...initReq, method: "POST", body: JSON.stringify(req)})
+  }
+}
