61edd457302e0014ee402aa1296e7e9f2b5c4d40
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-01-24 13:34:22 -0800

    chore: remove legacy llmoperator related code (#63)

--- dist/api/v1/legacy/cluster_manager_service.pb.d.ts
+++
@@ -1,8 +0,0 @@
-import * as fm from "../../../fetch.pb";
-import * as LlmarinerClustersServerV1Cluster_manager_service from "../cluster_manager_service.pb";
-export declare class ClustersInternalService {
-    static ListInternalClusters(req: LlmarinerClustersServerV1Cluster_manager_service.ListInternalClustersRequest, initReq?: fm.InitReq): Promise<LlmarinerClustersServerV1Cluster_manager_service.ListInternalClustersResponse>;
-}
-export declare class ClustersWorkerService {
-    static GetSelfCluster(req: LlmarinerClustersServerV1Cluster_manager_service.GetSelfClusterRequest, initReq?: fm.InitReq): Promise<LlmarinerClustersServerV1Cluster_manager_service.Cluster>;
-}
